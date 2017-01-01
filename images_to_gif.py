23b05d5d41dcbfc00ff6d72bae90ba1ec83240ad
weitang114 <weitang114@gmail.com>
2017-01-02 03:51:46 +0800

    remove useless imports

--- images_to_gif.py
+++ images_to_gif.py
@@ -1,10 +1,6 @@
 # -*- coding=utf-8 -*-
 import subprocess as sp
 from PIL import Image
-import imageio
-import images2gif
-import visvis.vvmovie.images2gif as images2gif_visvis
-import moviepy 
 
 def images_to_gif(gif_path, image_paths, duration_ms, method='imagemagick'):
     if method == 'imagemagick':
@@ -32,6 +28,7 @@ def images_to_gif_images2gif(gif_path, image_paths, duration_ms):
     """
     Some blocks at the edge are weird.
     """
+    import images2gif
     ims = [Image.open(p) for p in image_paths]
     images2gif.writeGif(gif_path, ims, duration=duration_ms / 1000., dither=1, dispose=2)
 
@@ -40,6 +37,7 @@ def images_to_gif_imageio(gif_path, image_paths, duration_ms):
     http://stackoverflow.com/questions/753190/programmatically-generate-video-or-animated-gif-in-python
     the speed is very weird
     """
+    import imageio
     print 'imageio. duration:', duration_ms / 1000.
     images = [imageio.imread(p) for p in image_paths]
     imageio.mimsave(gif_path, images, duration=duration_ms / 1000.)
@@ -49,6 +47,7 @@ def images_to_gif_moviepy(gif_path, image_paths, duration_ms):
     """
     just fails with some error..........
     """
+    import moviepy 
     my_clip = moviepy.editor.ImageSequenceClip(image_paths, fps=1./duration_ms)
     my_clip.write_gif(gif_path)
 
